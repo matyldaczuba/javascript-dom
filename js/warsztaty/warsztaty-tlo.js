0ae24c4b5547d5f6ff9b65e0a0bab33388c53a1c
Matylda Czuba <matylda.czuba@pm.me>
2018-10-07 15:40:53 +0200

    podstawowa struktura

---
+++ js/warsztaty/warsztaty-tlo.js
@@ -0,0 +1,11 @@
+let przycisk = document.getElementsByTagName("button");
+let p1 = document.querySelector(".pierwszy");
+let p2 = document.querySelector(".drugi");
+
+
+
+function ustawTlo(){
+    
+}
+
+przycisk.addEventListener("onclick", ustawTlo)
