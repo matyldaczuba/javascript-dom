732418660337e35f81440f149784fd0a99690c2d
Matylda Czuba <matylda.czuba@pm.me>
2018-10-07 11:05:07 +0200

    dom

---
+++ js/1-model-dom.js
@@ -0,0 +1,8 @@
+//
+//console.log(window);
+//
+//console.log(document);
+//
+//console.log(document.documentElement);
+
+console.log(document.head);
---
+++ js/2-znajdywanie-elementow.js
@@ -0,0 +1,13 @@
+let parFirst = document.getElementById("parFirst");
+console.log(parFirst);
+
+let link1 = document.getElementsByClassName("link")[0];
+console.log(link1);
+
+let paragrafy = document.getElementsByTagName("p");
+
+console.log(paragrafy);
+
+let onlyLink = document.querySelector(".link");
+console.log(onlyLink);
+
